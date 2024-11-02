ffe164cc793cdef4297248caa442af5e4735d1fa
= <=>
2024-11-02 15:46:31 +0100

    adapt generate to mac using mps

--- generate.py
+++ generate.py
@@ -1,47 +1,47 @@
-import torch 
+import torch
 import torchvision
 import os
 import argparse
-
+from tqdm import tqdm
 
 from model import Generator
 from utils import load_model
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate Normalizing Flow.')
-    parser.add_argument("--batch_size", type=int, default=2048,
-                      help="The batch size to use for training.")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate Normalizing Flow.")
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=2048,
+        help="The batch size to use for training.",
+    )
     args = parser.parse_args()
 
-
-
-
-    print('Model Loading...')
+    print("Model Loading...")
     # Model Pipeline
     mnist_dim = 784
 
-    model = Generator(g_output_dim = mnist_dim).cuda()
-    model = load_model(model, 'checkpoints')
-    model = torch.nn.DataParallel(model).cuda()
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+    model = Generator(g_output_dim=mnist_dim).to(device)
+    model = load_model(model, "checkpoints")
     model.eval()
 
-    print('Model loaded.')
+    print("Model loaded.")
 
+    print("Start Generating")
+    os.makedirs("samples", exist_ok=True)
 
+    total_samples = 10000
+    batch_size = args.batch_size
 
-    print('Start Generating')
-    os.makedirs('samples', exist_ok=True)
-
-    n_samples = 0
     with torch.no_grad():
-        while n_samples<10000:
-            z = torch.randn(args.batch_size, 100).cuda()
+        for n_samples in tqdm(range(0, total_samples, batch_size)):
+            z = torch.randn(batch_size, 100, device=device)
             x = model(z)
-            x = x.reshape(args.batch_size, 28, 28)
+            x = x.reshape(batch_size, 28, 28)
             for k in range(x.shape[0]):
-                if n_samples<10000:
-                    torchvision.utils.save_image(x[k:k+1], os.path.join('samples', f'{n_samples}.png'))         
-                    n_samples += 1
-
-
-    
+                if n_samples + k < total_samples:
+                    torchvision.utils.save_image(
+                        x[k : k + 1], os.path.join("samples", f"{n_samples + k}.png")
+                    )
